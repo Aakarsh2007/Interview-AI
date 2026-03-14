23648747384b21cea25bbec71b0fc26bd9de2f86
here2rule <aakarshsaxenaofficial@gmail.com>
2026-03-15 02:09:02 +0530

    Changed sendEmailjs to work on render

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,9 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com", 
-    port: 465,
-    secure: true,
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
@@ -23,9 +23,8 @@ async function sendEmail(to, subject, text) {
         return info;
     } catch (error) {
         console.error("NODEMAILER CRITICAL ERROR:", error);
-        throw error; 
+        throw error;
     }
 }
 
 module.exports = sendEmail;
-
